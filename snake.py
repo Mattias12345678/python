c01608f1906a33e84af24bffc1d112859104c896
Mattias Flores Menacho <matflo061118@student.jenseneducation.se>
2023-04-04 14:30:18 +0200

    första version av mitt prjekt

---
+++ snake.py
@@ -0,0 +1,183 @@
+import turtle   # gör så att jag kan göra/skriva vad jag vill i en window
+import time     # gör så att tiden, scoren siffror och sånt kommer upp i windowen
+import random
+
+delay = 0.1
+
+score = 0
+high_score = 0
+
+# Seting up the screen
+win = turtle.Screen()
+win.title("Snake Game by Mattias Tei22a")
+win.bgcolor("purple")
+win.setup(width=600, height=600)
+win.tracer(0)
+
+# Snake head
+head = turtle.Turtle()
+head.speed(0)
+head.shape("square")
+head.color("blue")
+head.penup()
+head.goto(0, 0)
+head.direction = "stop"
+
+# Snake food
+food = turtle.Turtle()
+food.speed(0)
+food.shape("circle")
+food.color("red")
+food.penup()
+food.goto(0, 100)
+
+segments = []
+
+# Ord
+pen = turtle.Turtle()
+pen.speed(0)
+pen.shape("square")
+pen.color("black")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Score: 0  High Score: 0", align="center", font=("Courier", 24, "normal"))
+
+
+# Functions
+def go_up():
+    if head.direction != "down":
+        head.direction = "up"
+
+
+def go_down():
+    if head.direction != "up":
+        head.direction = "down"
+
+
+def go_left():
+    if head.direction != "right":
+        head.direction = "left"
+
+
+def go_right():
+    if head.direction != "left":
+        head.direction = "right"
+
+
+def move():
+    if head.direction == "up":
+        y = head.ycor()
+        head.sety(y + 20)
+
+    if head.direction == "down":
+        y = head.ycor()
+        head.sety(y - 20)
+
+    if head.direction == "left":
+        x = head.xcor()
+        head.setx(x - 20)
+
+    if head.direction == "right":
+        x = head.xcor()
+        head.setx(x + 20)
+
+
+# keyboard bindings
+win.listen()
+win.onkeypress(go_up, "w")
+win.onkeypress(go_down, "s")
+win.onkeypress(go_left, "a")
+win.onkeypress(go_right, "d")
+
+# main game loop
+while True:
+    win.update()
+
+    # collision with the border
+    if head.xcor() > 290 or head.xcor() < -290 or head.ycor() > 290 or head.ycor() < -290:
+        time.sleep(1)
+        head.goto(0, 0)
+        head.direction = "stop"
+
+        # hiding the segments
+        for segment in segments:
+            segment.goto(1000, 1000)
+
+        # clear segments list
+        segments.clear()
+
+        # reset score
+        score = 0
+
+        # delay
+        delay = 0.1
+
+        pen.clear()
+        pen.write("Score: {}  High Score: {}".format(score, high_score), align="center", font=("Courier", 24, "normal"))
+
+        # check for a collision with the food
+    if head.distance(food) < 20:
+        # move the food to a random spot
+        x = random.randint(-290, 290)
+        y = random.randint(-290, 290)
+        food.goto(x, y)
+
+        # add a segment
+        new_segment = turtle.Turtle()
+        new_segment.speed(0)
+        new_segment.shape("square")
+        new_segment.color("grey")
+        new_segment.penup()
+        segments.append(new_segment)
+
+        delay -= 0.001
+
+        score += 1
+
+        if score > high_score:
+            high_score = score
+
+        pen.clear()
+        pen.write("Score: {}  High Score: {}".format(score, high_score), align="center", font=("Arial", 24, "normal"))
+
+        # move the end segments first in reverse order
+    for index in range(len(segments) - 1, 0, -1):
+        x = segments[index - 1].xcor()
+        y = segments[index - 1].ycor()
+        segments[index].goto(x, y)
+
+    # move segment 0 to where the head is
+    if len(segments) > 0:
+        x = head.xcor()
+        y = head.ycor()
+        segments[0].goto(x, y)
+
+    move()
+
+    # checking for head collision with the body segments
+    for segment in segments:
+        if segment.distance(head) < 20:
+            time.sleep(1)
+            head.goto(0, 0)
+            head.direction = "stop"
+
+            # hide the segments
+            for segment in segments:
+                segment.goto(1000, 1000)
+
+            # clear the segments list
+            segments.clear()
+
+            # reset the score
+            score = 0
+
+            # reset the delay
+            delay = 0.1
+
+            # update the score display
+            pen.clear()
+            pen.write("Score: {}  High Score: {}".format(score, high_score), align="center",
+                      font=("Arial", 24, "normal"))
+
+    time.sleep(delay)
